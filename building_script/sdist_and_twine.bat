71df79f46891395d9d8d18171c96cdd9ac5c5cf6
Andrew McSparron <Amcsparron@albanyny.gov>
2023-09-18 13:47:15 -0400

    PyPi Essentials

---
+++ building_script/sdist_and_twine.bat
@@ -0,0 +1,12 @@
+@echo off
+
+cd C:\Users\amcsparron\Desktop\Python_Projects\IndexedChoiceSelector
+echo pwd changed to C:\Users\amcsparron\Desktop\Python_Projects\IndexedChoiceSelector
+REM need LICENSE.txt README.md setup.cfg setup.py - see https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
+REM DONT FORGET TO UPDATE setup.py, push commit to remote, and create a new release!! THEN run this!!!
+echo running sdist setup
+python setup.py sdist
+
+echo running twine to update pypi
+twine upload dist/*
+
